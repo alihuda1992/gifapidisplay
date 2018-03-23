7354d40ed7808e0754a89832da908d37c5967208
Ali Huda <alihuda1992@gmail.com>
2018-03-22 23:31:28 -0400

    updating

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 var category = ["Apple", "Banana", "Pear", "Orange", "Pineapple", "Grapefruit", "Watermelon", "Strawberry", "Lemon"];
 var search;
-console.log(category)
-
 
 
 
@@ -11,8 +9,34 @@ $(document).ready(function () {
         $("#pillcontainer").append('<li role="presentation" class="pill" value="' + category[i] + '"><a>' + category[i] + '</a></li>')
     }
 
-   
+    $("#fruitbutton").click(function() {
+     $("#container").html("");
+    category.push($(".form-control").val());
+    $("#pillcontainer").append('<li role="presentation" class="pill" value="' + $(".form-control").val() + '"><a>' + $(".form-control").val() + '</a></li>')
+    $ ("#container").append('<img src =' + $(".form-control").val()  + '>')
+    
 
+    var xhr = $.get("https://api.giphy.com/v1/gifs/search?q=" + $(".form-control").val() + "&api_key=ZK9v6ITzVl3nm5Mz8iPUch43jDnk5b1v&limit=10");
+    xhr.done(function (response) {
+        for (var i = 0; i < response.data.length; i++) {
+
+            $("#container").append('<img src =' + response.data[i].images.original.url + '>')
+        }
+           
+       
+    //close ajax 
+    });
+
+
+
+
+
+
+
+
+
+
+});
 
     $(".pill").click(function () {
         var keyword = $(this).attr("value");
@@ -22,7 +46,6 @@ $(document).ready(function () {
         //AJAX call and for loop to print images from response of call
         var xhr = $.get("https://api.giphy.com/v1/gifs/search?q=" + keyword + "&api_key=ZK9v6ITzVl3nm5Mz8iPUch43jDnk5b1v&limit=10");
         xhr.done(function (response) {
-            console.log("success got data", response);
             for (var i = 0; i < response.data.length; i++) {
 
                 $("#container").append('<img src =' + response.data[i].images.original.url + '>')
